98169c89e986904e329ad86db20834ef7d42b35d
Michael Appiah Dankwah <117749434+Terre8055@users.noreply.github.com>
2023-07-09 14:22:34 +0000

    Fix fabric importation

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,11 +2,10 @@
 """A module for Fabric script that generates a .tgz archive."""
 import os
 from datetime import datetime
-from fabric.api import local, runs_once
+from fabric import task
 
-
-@runs_once
-def do_pack():
+@task
+def do_pack(c):
     """Archives the static files."""
     if not os.path.isdir("versions"):
         os.mkdir("versions")
@@ -21,9 +20,10 @@ def do_pack():
     )
     try:
         print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
+        c.run("tar -cvzf {} web_static".format(output))
         size = os.stat(output).st_size
         print("web_static packed: {} -> {} Bytes".format(output, size))
+        print("Done")
     except Exception:
         output = None
     return output
